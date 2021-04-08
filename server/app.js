4a79900c29f4596b878296ce21a9e46bbb2203f0
sumit patil <sumitpatil90990@gmail.com>
2021-04-08 10:37:55 +0530

    understanding middle ware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 
-
 const port = process.env.PORT || 3000;
+
+const customMidleware = function(req, res, next){
+    console.log("midleware")
+    next()
+}
+
+// app.use(customMidleware);
 
 app.route('/')
 .get(function(req, res){
     res.send("hello world");
+})
+
+
+app.route("/about")
+.get(customMidleware,function(req, res){
+    res.send("About Page")
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
