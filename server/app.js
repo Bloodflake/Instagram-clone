83892f684ecdd67a61cf8abefa1e8801e5e05c91
sumit patil <sumitpatil90990@gmail.com>
2021-04-08 10:18:52 +0530

    initial files

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+app.route('/')
+.get(function(req, res){
+    res.send("hello world");
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
