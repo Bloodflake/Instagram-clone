79d4c5003e0c7c44953f1bd0e926fdb42dda7c1e
sumit patil <sumitpatil90990@gmail.com>
2021-04-09 12:23:30 +0530

    added bodyParser dependency

--- server/app.js
+++ server/app.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
 dotenv.config();
 
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(require("./routes/auth"));
+
+
 mongoose.connect(`mongodb+srv://sumit:${process.env.PWSSRD}@cluster0.5zven.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
 { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.on("connected", ()=>{
+mongoose.connection.on("connectedccd", ()=>{
     console.log("connected to DB")
 });
 
@@ -17,10 +24,10 @@ mongoose.connection.on("error", (err)=>{
     console.log("error", err)
 })
 
-app.route('/')
-.get(function(req, res){
-    res.send("hello world");
-})
+// app.route('/')
+// .get(function(req, res){
+//     res.send("hello world");
+// })
 
 
 app.listen(port, () => console.log(`Server running on port ${port} ${process.env.USER}`));
